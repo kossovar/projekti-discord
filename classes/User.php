15302bdcef7738ba4eb9be122fe26dce2009891d
Elsa <er68090@ubt-uni.net>
2024-01-25 21:11:50 +0100

    Add user class

---
+++ classes/User.php
@@ -0,0 +1,30 @@
+<?php
+
+class User {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function isUsernameTaken($username) {
+        $query = "SELECT * FROM users WHERE username = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->num_rows > 0;
+    }
+
+    public function isEmailTaken($email) {
+        $query = "SELECT * FROM users WHERE email = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->num_rows > 0;
+    }
+
+}
+
+?>
