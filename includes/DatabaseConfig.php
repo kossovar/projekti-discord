6b589d0ebc73388d15d26d549f6ae76cc2c25a45
Elsa <er68090@ubt-uni.net>
2024-01-25 21:09:03 +0100

    Add database config file to store credentials

---
+++ includes/DatabaseConfig.php
@@ -0,0 +1,19 @@
+<?php
+require_once('./classes/Database.php');
+
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "discord";
+
+$database = new Database($servername, $username, $password, $dbname);
+
+$conn = $database->getConnection();
+
+if ($conn) {
+    return $database;
+} else {
+    echo "Connection failed!";
+}
+
+?>
